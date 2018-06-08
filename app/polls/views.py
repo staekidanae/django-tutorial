e65160295f791fe623d6ece7e4feac2489dfa465
staekidanae <staekidanae@gmail.com>
2018-06-08 10:46:11 +0900

    index에서 detail로의 링크에 url 태그 사용

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Question
 from django.template import loader
+
 
 def index(request):
     # 최신 질문들을 정리하는데 pub_date를 역순으로 0~4번째까지
@@ -16,12 +17,29 @@ def index(request):
     # output을 웹 응답으로 송신 및 출력
     return render(request, 'polls/index.html', context)
 
+def custom_get_object_or_404(model, **kwargs):
+    try:
+        return model.objects.get(**kwargs)
+    except model.DoesNotExist:
+        raise Http404()
+
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+
+    question = custom_get_object_or_404(Question, id=question_id)
+    context = {
+        'question': question,
+    }
+    # return HttpResponse("You're looking at question %s." % question_id)
+    return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
+    
     return HttpResponse("You're voting on question %s." % question_id)
